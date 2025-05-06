521c0db5c02eced4974a7ee2d53f3ab075ace6eb
Jala Aguirre <jbaguirre1@up.edu.ph>
2025-05-07 02:16:07 +0800

    [JALA] Toast Notifs
    modify the request event button into a component, incorporated a success toast alongside the success message when submitting a form.

--- src/app/user/home/page.tsx
+++ src/app/user/home/page.tsx
@@ -1,19 +1,37 @@
 "use client";
 
 import * as React from "react";
+<<<<<<< HEAD
 import { EventRequestDialog } from "@/components/ui/event-request-dialog";
 import { EventDetails } from "@/components/left-event-details";
+=======
+import { Button } from "@/components/ui/button";
+import { EventRequestDialog } from "@/components/user/request-events/event-request-dialog";
+>>>>>>> f887645 ([JALA] Toast Notifs)
 
 export default function UserHomePage() {
-  const [open, setOpen] = React.useState(false);
+  const [openDialog, setOpenDialog] = React.useState(false);
 
   return (
     <div className="flex justify-center items-center min-h-screen">
+<<<<<<< HEAD
       <EventRequestDialog open={open} setOpen={setOpen} />
     </div> 
+=======
+      <div className="flex gap-4">
+        <Button
+          className="bg-[#284b3e] text-white hover:bg-[#284b3e]/90"
+          onClick={() => setOpenDialog(true)}
+        >
+          Request an Event
+        </Button>
+      </div>
+
+      <EventRequestDialog open={openDialog} onOpenChange={setOpenDialog} />
+    </div>
+>>>>>>> f887645 ([JALA] Toast Notifs)
   );
 }
-
 
 // revamped, calnedar still not working tho
 // "use client";
