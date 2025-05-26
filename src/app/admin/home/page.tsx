d4eae26b54c6303c6461f81f6528c36d0069bead
Jala Aguirre <jbaguirre1@up.edu.ph>
2025-05-27 01:50:09 +0800

    [Jala] Update admin dashboard

--- src/app/admin/home/page.tsx
+++ src/app/admin/home/page.tsx
@@ -2,7 +2,7 @@ import AdminDashboard from "@/components/admin/AdminDashboard";
 
 export default function AdminHome() {
   return (
-    <div className="p-6">
+    <div className="flex-1 h-full overflow-auto p-6 flex justify-center items-start">
       <AdminDashboard />
     </div>
   );
